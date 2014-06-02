7901e15d1bc88d47c3138c3642c13ebb94d65307
carpedm20@gmail.com <carpedm20@gmail.com>
2014-06-02 20:07:09 +0900

    minor fix for json import

--- movietag/import_from_json.py
+++ movietag/import_from_json.py
@@ -1,30 +1,77 @@
+#-*- coding: utf-8 -*-
 from movie.models import *
 import json
+
+def sub_genre_to_normal(sub_genre):
+    genre_list = sub_genre.split('_')
+    new_list = []
+    for g in genre_list:
+        g = g.strip()
+
+        if g in ['로맨스/멜로', '로맨틱', '멜로', '멜로/로맨스', '멜로/애정/로맨스']:
+            new_list.append('로맨스')
+        elif g in ['서스펜스']:
+            new_list.append('스릴러')
+        elif g in ['코메디']:
+            new_list.append('코미디')
+
+    return new_list
+
+def main_genre_to_normal(main_genre):
+    main_genre = main_genre.strip()
+
+    if main_genre == '로맨스/멜로':
+        return '로맨스'
+
+    return main_genre
 
 f = open('movie_tag.json','r')
 j=json.loads(f.read())
 f.close()
 
+count = 0
 for i in j['data']:
     info = i['info']
     tag_list = i['tags']
+
+    count += 1
+    print "[%s] %s" % (count, info['title'])
+
+    main_genre = main_genre_to_normal(info['main_genre'])
+    sub_genre = sub_genre_to_normal(info['sub_genre'])
+
+    try:
+        main_genre = Genre.objects.get(text=main_genre)
+    except:
+        main_genre = Genre(text=main_genre)
+
+    sub_genre_list = []
+    for sub in sub_genre:
+        try:
+            sub_obj = Genre.objects.get(text=sub)
+        except:
+            sub_obj = Genre(text=sub)
+
+        sub_genre_list.append(sub_obj)
 
     m = Movie(title = info['title'],
               title_aka = info['title_aka'],
               title_eng = info['title_eng'],
               title_url = info['title_url'],
-              main_genre = info['main_genre'],
-              sub_genre = info['sub_genre'],
               year = info['year'],
               running_time = info['running_time'],
               released_at = info['released_at'],
               re_released_at = info['re_released_at'],
-              other_raiting = info['raiting'],
-              poster_big = info['poster_big'],
-              stillcut_big = info['poster_big'],
+              other_rating = info['watcha_rating'],
+              poster_big = info['poster']['big'],
+              stillcut_big = info['stillcut']['big'],
               youtube_id = info['youtube_id'])
-
     m.save()
+
+    m.main_genre = main_genre
+
+    for sub in sub_genre_list:
+        m.sub_genre_set.add(sub)
 
     for tag in tag_list:
         text = tag['text']
--- movietag/movie/models.py
+++ movietag/movie/models.py
@@ -4,7 +4,7 @@ import base64
 
 from django.db import models
 
-from Account.models import Account
+from account.models import Account
 
 # Create your models here.
 class Movie(models.Model):
@@ -13,38 +13,47 @@ class Movie(models.Model):
     title_eng = models.CharField(max_length=100)
     title_url = models.SlugField(max_length=50)
 
-    main_genre = models.CharField(max_length=30, blank=True)
-    sub_genre = models.CharField(max_length=30, blank=True)
+    main_genre = models.CharField(max_length=30, null=True, blank=True)
+    sub_genre = models.CharField(max_length=30, null=True, blank=True)
 
     year = models.IntegerField(default=-1)
     running_time = models.IntegerField(default=-1)
     released_at = models.DateField(null=True)
     re_released_at = models.DateField(null=True)
 
-    raiting = models.FloatField(default=0)
-    other_raiting = models.FloatField(default=0)
+    rating = models.FloatField(default=0)
+    other_rating = models.FloatField(default=0)
 
-    poster_big = models.URLField(blank=True)
-    stillcut_big = models.URLField(blank=True)
+    poster_big = models.URLField(null=True, blank=True)
+    stillcut_big = models.URLField(null=True, blank=True)
 
-    youtube_id = models.CharField(max_length=30, blank=True)
+    youtube_id = models.CharField(max_length=30, null=True, blank=True)
 
     unique_id = models.CharField(max_length=10)
 
+    main_genre = models.ForeignKey('Genre')
+    sub_genre_set = models.ManyToManyField('Genre', related_name='sub_genre')
+    
     def save(self, *args, **kwargs):
         if not self.unique_id:
             while True:
                 unique_id = base64.b64encode(str(uuid.uuid4()))[:10]
-                group = StudyGroup.objects.filter(unique_id=unique_id)
+                m = Movie.objects.filter(unique_id=unique_id)
 
-                if len(group) is 0:
+                if len(m) is 0:
                     self.unique_id = unique_id
                     break
 
-        super(StudyGroup, self).save(*args, **kwargs)
+        super(Movie, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return "%s (%s)" % (self.title, self.title_eng)
+
+class Genre(models.Model):
+    text = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return "%s" % (self.text)
 
 class Tag(models.Model):
     text = models.CharField(max_length=30)
@@ -53,13 +62,13 @@ class Tag(models.Model):
     like_user = models.ManyToManyField(Account)
 
     def __unicode__(self):
-        return "%s" % (self.title)
+        return "%s" % (self.text)
 
 class Frequency(models.Model):
-    tag = models.ForiegnKey(Tag)
-    movie = models.ForiegnKey(Movie)
+    tag = models.ForeignKey(Tag)
+    movie = models.ForeignKey(Movie)
 
     freq = models.IntegerField(default=0)
 
     def __unicode__(self):
-        return "%s - %s" % (tag.title, self.freq)
+        return "%s - %s" % (tag.text, self.freq)
