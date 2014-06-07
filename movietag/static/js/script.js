59bc6c6e756b950a06a671cebfda9dbb93374494
carpedm20@gmail.com <carpedm20@gmail.com>
2014-06-07 18:24:27 +0900

    minor update

--- movietag/static/js/script.js
+++ movietag/static/js/script.js
@@ -1,6 +1,14 @@
 $( document ).ready(function() {
   var $container = $('.content');
 
+  /*$container.imagesLoaded( function() {
+    $container.masonry({
+      itemSelector        : '.poster',
+      columnWidth         : '.poster',
+      transitionDuration  : 0
+    });
+  });*/
+
   $(this).find(".poster a.frame").twipsy({delayIn:1500,offset:1});
 
   $('.raty').raty({
